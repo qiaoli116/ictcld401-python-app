7d96f118814901f869ad2ad03925c9e3ecb973d5
qiaoli116 <qiaoli.116@gmail.com>
2023-05-08 00:30:56 +1000

    init

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+# Obtain a session token
+def obtainSessionToken():
+    # Obtain a session token from the metadata service
+    TOKEN_URL = 'http://169.254.169.254/latest/api/token'
+    TOKEN_TTL = '21600'
+    TOKEN_HEADER = {'X-aws-ec2-metadata-token-ttl-seconds': TOKEN_TTL}
+    token_response = requests.put(TOKEN_URL, headers=TOKEN_HEADER)
+    session_token = token_response.text
+    return session_token
+
+# Retrieve the private IP address using the session token
+def retrivePrivateIP():
+    session_token = obtainSessionToken()
+    # Retrieve the private IP address using the session token
+    PRIVATE_IP_URL = 'http://169.254.169.254/latest/meta-data/local-ipv4'
+    IP_HEADER = {'X-aws-ec2-metadata-token': session_token}
+    ip_response = requests.get(PRIVATE_IP_URL, headers=IP_HEADER)
+    private_ip = ip_response.text
+    return private_ip
+
+@app.route('/')
+def index():
+
+    
+    private_ip = retrivePrivateIP()
+    message = {'private_ip': private_ip}
+    return render_template('index.html', message=message)
+
+if __name__ == '__main__':
+    # run the http server with port 8080
+    app.run(8080)
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Flask Web App
+This is a simple Flask web application that displays information on the AWS EC2 server. Note this app must be running on an AWS Linux server.
+
+## Getting Started
+To run the app, you must have Python 3.x and the Flask library installed on your system. You can install Flask using pip by running the following command:
+
+```bash
+pip install Flask
+```
+
+Once you have installed Flask, you can run the app using the following command:
+
+```bash
+python app.py
+```
+
+This will start the Flask development server, and the app will be accessible in your web browser at http://<domain>:8080/.
+
+## Checking the Result
+If the app runs correctly, you should see a web page displayed in your web browser.
+
+## Contact
+If you have any questions about this project, feel free to contact me at <qiao.li@holmesglen.edu.au>.
+
+## License
+This project is licensed under the MIT License.
